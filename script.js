49203fac3470a2ff3404ccb42fb6aba2fe8ca434
Eduarddoribeiro <eduardoribeirodefraga05@gmail.com>
2024-01-15 15:13:33 -0300

    adjust container width

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function toggleMode() {
-    const html document.documentElement
+    const html = document.documentElement
     html.classList.toggle("light")
+
+    const img = document.querySelector("#profile img")
+
     if(html.classList.contains('light')) {
+       
 
     } else {
 
